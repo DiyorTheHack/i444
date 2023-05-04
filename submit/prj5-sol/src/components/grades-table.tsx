e2b750ebd147ac2b15d94be3cc311020d30a6206
Diyorjon Makhkamov <dmakhka1@CS444-dmakhka1.bu.binghamton.edu>
2023-05-03 23:20:40 -0400

    finished prj5

--- submit/prj5-sol/src/components/grades-table.tsx
+++ submit/prj5-sol/src/components/grades-table.tsx
@@ -15,9 +15,54 @@ type GradesTableProps = {
   setResult: (result: Result<G.Grades>) => void,
 };
 
+type Patches = {
+  [rowId: string]: { [colId: string]: G.RawData }
+};
+
+
 export default function GradesTable(props: GradesTableProps) {
   const { ws, courseId, courseInfo, grades, setResult } = props;
-  return <>TODO</>;
+
+  // Pull out the full-table data rows array from the grades prop
+  const dataRows = grades.getFullTable();
+
+  // If there are no rows in the array, simply return an empty <table> with an empty <tbody>
+  if (dataRows.length === 0) {
+    return (
+      <table>
+        <tbody>
+          <Header hdrs={[]} />
+        </tbody>
+      </table>
+    );
+  }
+  
+  const changeGradeHandler = (rowId: string, colId: string, val: string) => {
+    if (val !== "" && isNaN(Number(val))) {
+      setResult(errResult(`Invalid grade value: ${val}`));
+      return;
+    }
+  
+    const patches: Patches = { [rowId]: { [colId]: val } };
+  
+    ws.updateCourseGrades(courseId, patches)
+      .then((result: Result<G.Grades>) => setResult(result));
+  };
+  
+
+  return (
+    <table>
+      <tbody>
+        <Header hdrs={Object.keys(dataRows[0])} />
+        <DataTable
+          data={dataRows}
+          courseInfo={courseInfo}
+          changeGrade={changeGradeHandler}
+        />
+      </tbody>
+    </table>
+  );
+  
 }
 
 /* The following sub-components are based on the visual layout of
@@ -50,8 +95,17 @@ type HeaderProps = {
 };
 
 function Header(props: HeaderProps) {
-  return <></>;
+  const { hdrs } = props;
+
+  return (
+    <tr>
+      {hdrs.map((header) => (
+        <th key={header}>{header}</th>
+      ))}
+    </tr>
+  );
 }
+
 
 type DataTableProps = {
   data: G.GradeRow[],
@@ -61,8 +115,21 @@ type DataTableProps = {
 
 function DataTable(props: DataTableProps) {
   const { data, courseInfo, changeGrade } = props;
-  return <></>;
+  
+  return (
+    <>
+      {data.map((dataRow, index) => (
+        <DataRow
+          key={index}
+          dataRow={dataRow}
+          courseInfo={courseInfo}
+          changeGrade={changeGrade}
+        />
+      ))}
+    </>
+  );
 }
+
 
 type DataRowProps = {
   dataRow: G.GradeRow,
@@ -71,9 +138,50 @@ type DataRowProps = {
 };
 
 function DataRow(props: DataRowProps) {
-  const {dataRow, courseInfo, changeGrade} = props;
-  return <></>;
+  const { dataRow, courseInfo, changeGrade } = props;
+
+  const rowId = dataRow[courseInfo.rowIdColId]?.toString() || '';
+
+  const formatVal = (value: G.Grade) => {
+    if (typeof value === 'number') {
+      return value.toFixed(1);
+    }
+    return value.toString();
+  };
+
+  const isEditable = (rowId: string, colId: string) => {
+    return (
+      rowId &&
+      courseInfo.cols[colId]?.kind === 'score' &&
+      colId !== courseInfo.rowIdColId
+    );
+  };
+
+  //const rowId = dataRow[courseInfo.rowIdColId];
+
+  return (
+    <tr>
+      {Object.entries(dataRow).map(([colId, value]) => {
+        if (isEditable(rowId, colId)) {
+          return (
+            <td key={colId}>
+              <GradeInput
+                rowId={rowId}
+                colId={colId}
+                val={formatVal(value)}
+                changeGrade={changeGrade}
+              />
+            </td>
+          );
+        } else {
+          return <td key={colId}>{formatVal(value)}</td>;
+        }
+      })}
+    </tr>
+  );
 }
+
+
 
 type GradeInputProps = {
   rowId: string,
@@ -84,5 +192,21 @@ type GradeInputProps = {
 
 function GradeInput(props: GradeInputProps) {
   const { rowId, colId, val, changeGrade } = props;
-  return <></>;
+
+  const [value, setValue] = React.useState(val);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    if (value !== val) {
+      changeGrade(rowId, colId, value);
+    }
+  };
+
+  return (
+    <input size={3} value={value} onChange={handleChange} onBlur={handleBlur} />
+  );
 }
+
